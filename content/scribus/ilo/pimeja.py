3af2e97296932eab3afc258b70993da3a7b57d81
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2025-11-14 23:26:45 +0300

    scribus: pimeja: implement image swapping

--- content/scribus/ilo/pimeja.py
+++ content/scribus/ilo/pimeja.py
@@ -1,6 +1,9 @@
 # makes the black-and-white version
 
+from pathlib import Path
+
 import scribus
+
 
 def replace_main_colors():
 	for color in ['loje', 'laso', 'jelo', 'unu']:
@@ -28,29 +31,29 @@ def replace_cover_colors():
 	raise NotImplementedError
 
 def replace_front_image():
-	scribus.goToPage(1)
+	scribus.gotoPage(1)
 	raise NotImplementedError
 
 def replace_images():
 	# iterate over all image frames
 	for page in range(1, scribus.pageCount() + 1):
-		scribus.goToPage(page)
+		scribus.gotoPage(page)
 		for item in scribus.getPageItems():
 			if item[1] != 2: continue
 
-			filename = scribus.getImageFile(item[0]) # OR getImageName. docs unclear
+			filename = Path(scribus.getImageFile(item[0])) # OR getImageName. docs unclear
 			x_scale, y_scale = scribus.getImageScale(item[0])
-			x_offset, y_offset = scribus.getImageOffset(x, y, name) # TODO DOES NOT EXIST?
+			x_offset, y_offset = scribus.getImageOffset(item[0])
 
 			# TODO if filename + pimeja exists, do …. else: nothing.
 			if False: continue
-			scribus.loadImage(filename + '-pimeja', name) # FIXME before the extension
-			scribus.setImageScale(x_scale, y_scale, name)
-			scribus.setImageOffset(x_offset, y_offset, name)
-	
+			scribus.loadImage(str(filename.with_name(filename.name.replace("-kule", "-pimeja"))), item[0]) # FIXME before the extension
+			scribus.setImageScale(x_scale, y_scale, item[0])
+			scribus.setImageOffset(x_offset, y_offset, item[0])
+
 
 if __name__ == '__main__':
-	# TODO option 1: save the file first, then replace colours and save the Scribus file in a new -pimeja.sla file.
+	# TODO option 1: save the file first, then replace colours and save the Scribus file in a new _bw.sla file.
 	choice = scribus.messageBox('Replace colours?', 'Do you want to replace the main colours with their greyscale variants?', scribus.ICON_NONE, scribus.BUTTON_CANCEL|scribus.BUTTON_ESCAPE, scribus.BUTTON_YES|scribus.BUTTON_DEFAULT, scribus.BUTTON_NO)
 	# TODO remove the cancel button. but then, the numbers might change.
 	if choice == 4194304:
@@ -58,8 +61,8 @@ if __name__ == '__main__':
 		pass
 	elif choice == 16384:
 		# yes
+		replace_images()
 		replace_main_colors()
 	elif choice == 65536:
 		# no
 		pass
-
